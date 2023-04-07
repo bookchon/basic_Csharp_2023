1472949c75a7dbf6af1fa349d94b7de12c939b2f
bookchon <realtansan@gmail.com>
2023-04-07 12:28:49 +0900

    2023. 4. 7.

---
+++ Day04/Day04ConsoleApp/cs17_ingeritance/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs17_ingeritance
+{
+    // 상속해 줄 부모 클래스
+    class Parent
+    {
+        public string Name;
+        public Parent(string Name)
+        {
+            this.Name = Name;
+            Console.WriteLine("{0} from Prent 생성자", Name);
+        }
+
+        public void ParentMethod()
+        {
+            Console.WriteLine("{0} from Prent 메서드", Name);
+        }
+    }
+
+    // 상속받을 자식 클래스
+    class Child : Parent
+    {
+        public Child(string Name) : base(Name) // 부모 생성자를 먼저 실행한 뒤 자신의 생성자를 실행
+        {
+            Console.WriteLine("{0} from Child 생성자", Name);
+        }
+
+        public void ChildMethod()
+        {
+            Console.WriteLine("{0} form Child 메서드", Name);
+        }
+    }
+
+    // 클래스 간 형변환
+    class Mammal
+    {
+        public void Nurse()
+        {
+            Console.WriteLine("포유류 기르다");
+        }
+    }
+
+    class Dogs : Mammal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("멍멍");
+        }
+    }
+
+    class Cats : Mammal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("야옹");
+        }
+    }
+
+    class Elephant : Mammal
+    {
+        public void Poo()
+        {
+            Console.WriteLine("뿌우우");
+        }
+    }
+
+    class ZooKeeper : Mammal
+    {
+        public void Wash(Mammal mammal)
+        {
+            if (mammal is Elephant)
+            {
+                var animal = mammal as Elephant;
+                Console.WriteLine("코끼리를 씻깁니다.");
+                animal.Poo();
+            }
+            else if (mammal is Dogs)
+            {
+                var animal = mammal as Dogs;
+                Console.WriteLine("강아지를 씻깁니다.");
+                animal.Bark();
+            }
+
+            else if (mammal is Cats)
+            {
+                var animal = mammal as Cats;
+                Console.WriteLine("고양이를 씻깁니다.");
+                animal.Meow();
+                animal.Meow();
+                animal.Meow();
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region < 기본 상속 개념 >
+            Parent p = new Parent("p");
+            p.ParentMethod();
+
+            Console.WriteLine("자식클래스 생성");
+            Child c = new Child("c");
+            c.ParentMethod(); // 자식 클래스는 부모 클래스의 속성 기능을 모두 쓸 수 있음. 단, public, protect일 때만
+            c.ChildMethod();
+            #endregion
+
+            #region < 클래스 간 형식 변환>
+            // Mammal mammal = new Mammal(); 기본
+            Mammal mammal = new Dogs();
+
+            if(mammal is Dogs)
+            {
+                Dogs midDog = mammal as Dogs; // (Dogs)mammal 구식 
+                midDog.Bark();
+            }
+            
+
+            // Dogs dogs = new Mammal(); 부모클래스가 자식 클래스로 변환은 불가함
+
+            #endregion
+
+            Dogs dog2 = new Dogs();
+            Dogs cat2 = new Dogs();
+
+            ZooKeeper keeper = new ZooKeeper();
+            keeper.Wash(dog2);
+            keeper.Wash(cat2);
+
+        }
+    }
+}
---
+++ Day04/Day04ConsoleApp/cs19_interface/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs19_interface
+{
+    interface ILogger
+    {
+        void Writelog(string log);
+    }
+
+    interface IFormattableLogger : ILogger
+    {
+        void WriteLog(string format, params object[] args); // 다중 파라미터
+    }
+
+    class ConsoleLogger : ILogger   // 인터페이스를 상속하는 것은 구현이라고 부름
+    {
+        public void Writelog(string log)
+        {
+            Console.WriteLine("{0} {1}", DateTime.Now.ToLocalTime(), log);
+        }
+    }
+
+    class Car
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public void Stop() 
+        {
+            Console.WriteLine("정지");
+        }
+    }
+
+    interface IHoverable
+    {
+        void Hover(); // 물에서 달림
+    }
+    interface IFlyable
+    {
+        void Fly(); // 날다
+    }
+
+    // C#은 다중상속이 없음
+    class FlyHoverCar : Car, IFlyable, IHoverable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("납니다.");
+        }
+
+        public void Hover()
+        {
+            Console.WriteLine("물에서 달립니다.");
+        }
+    }
+
+    class ConsoleLogger2 : IFormattableLogger
+    {
+        public void WriteLog(string format, params object[] args)
+        {
+            string message = string.Format(format, args);
+            Console.WriteLine("{0}, {1}", DateTime.Now.ToLocalTime(), message);
+        }
+
+        public void Writelog(string log)
+        {
+            Console.WriteLine("{0}, {1}", DateTime.Now.ToLocalTime(), log);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new ConsoleLogger();
+            logger.Writelog("안녕");
+
+            IFormattableLogger logger2 = new ConsoleLogger2();
+            logger2.WriteLog("{0} x {1} = {2}", 6, 5, (6 * 5));
+        }
+    }
+}
