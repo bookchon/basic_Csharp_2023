c77b372c02a79a99959f6f1a60d890988117bec9
bookchon <realtansan@gmail.com>
2023-04-17 13:43:44 +0900

    2023. 4. 17.

---
+++ Day08/WindowsFormsApp1/WindowsFormsApp1/FrmMain.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtNumber = new System.Windows.Forms.TextBox();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LblResult = new System.Windows.Forms.Label();
+            this.Worker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔고딕코딩", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "입력(수)";
+            // 
+            // TxtNumber
+            // 
+            this.TxtNumber.Location = new System.Drawing.Point(68, 9);
+            this.TxtNumber.Name = "TxtNumber";
+            this.TxtNumber.Size = new System.Drawing.Size(136, 21);
+            this.TxtNumber.TabIndex = 1;
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Font = new System.Drawing.Font("나눔고딕코딩", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnStart.Location = new System.Drawing.Point(217, 8);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(75, 23);
+            this.BtnStart.TabIndex = 2;
+            this.BtnStart.Text = "시작";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Font = new System.Drawing.Font("나눔고딕코딩", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnCancel.Location = new System.Drawing.Point(301, 8);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(75, 23);
+            this.BtnCancel.TabIndex = 3;
+            this.BtnCancel.Text = "취소";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 36);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(366, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("나눔고딕코딩", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "결과";
+            // 
+            // LblResult
+            // 
+            this.LblResult.AutoSize = true;
+            this.LblResult.Font = new System.Drawing.Font("나눔고딕코딩", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.LblResult.Location = new System.Drawing.Point(57, 73);
+            this.LblResult.Name = "LblResult";
+            this.LblResult.Size = new System.Drawing.Size(11, 12);
+            this.LblResult.TabIndex = 6;
+            this.LblResult.Text = "0";
+            // 
+            // Worker
+            // 
+            this.Worker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.Worker_DoWork);
+            this.Worker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.Worker_ProgressChanged);
+            this.Worker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.Worker_RunWorkerCompleted);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 101);
+            this.Controls.Add(this.LblResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.TxtNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "BackgroundWorker 테스트";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtNumber;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LblResult;
+        private System.ComponentModel.BackgroundWorker Worker;
+    }
+}
+
---
+++ Day08/WindowsFormsApp1/WindowsFormsApp1/FrmMain.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FrmMain : Form
+    {
+        int number = 0;
+        int percent = 0;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            Worker.WorkerSupportsCancellation = true;
+            Worker.WorkerReportsProgress = true;
+        }
+
+        // 백그라운드로 일 진행 // Thread.Sstart()
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker; // 인자 값으로 BackgroundWorker 생성
+
+            e.Result = Fibonacci((int)e.Argument, worker, e); // 피보나치 계산 메서드
+        }
+
+        private long Fibonacci(int arg, BackgroundWorker worker, DoWorkEventArgs e)
+        {
+            // arg는 0 ~ 91 사이. 91보다 크면 long 오버플로우 발생
+            if (arg < 0 || arg > 91)
+            {
+                throw new Exception("오류 0 ~ 91사이 입력");
+            }
+
+            long result = 0;
+
+            if (worker.CancellationPending == true)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                if (arg < 2)
+                {
+                    result = 1;
+                }
+                else
+                {
+                    // arg만 계산에 필요한 매개 변수, worker, e 중간에 취소할 때 필요한 BackgroundWorker 복사본
+                    result = Fibonacci(arg - 1, worker, e) + Fibonacci(arg - 2, worker, e);
+                }
+                //  진행사항을 프로그래스바에 표시
+                int percentComplete = (int)(arg / number * 100);
+                if (percentComplete > percent)
+                {
+                    percent = percentComplete;
+                    worker.ReportProgress(percentComplete); // ProgressChanged 이벤트 발생
+                }
+            }
+            return result;
+        }
+
+        // 백그라운드 스레드 채스크 종료 뒤 처리
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else if (e.Cancelled)
+            {
+                LblResult.Text = "취소됨";
+            }
+            else
+            {
+                LblResult.Text = e.Result.ToString();
+            }
+            TxtNumber.Text = 0.ToString();
+            BtnStart.Enabled = true;
+            BtnCancel.Enabled = false;
+        }
+
+        // 백그라운드 스레드 진행중 프로그래스
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            PgbCalculate.Value = e.ProgressPercentage; // 진행사항 표시
+        }
+
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            // Button button = sender as Button;
+            BtnStart.Enabled = false; // 시작버튼은 종료시까지 누르지 못함
+            BtnCancel.Enabled = true;
+
+            number = int.Parse(TxtNumber.Text);
+
+            percent = 0;
+            PgbCalculate.Value = percent;
+            Worker.RunWorkerAsync(number); // 비동기로 진행
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            Worker.CancelAsync();  // 비동기 취소 요청
+            BtnCancel.Enabled = false;
+            BtnStart.Enabled = true;
+        }
+    }
+
+}
